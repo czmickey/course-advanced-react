3ba3e22ad2e133615fb3e2ab7f566a7433a8bdd0
Michal Oktábec <info@michaloktabec.cz>
2021-03-24 18:38:29 +0100

    BE: Add seed data

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -6,6 +6,7 @@ import { withItemData, statelessSessions } from '@keystone-next/keystone/session
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
@@ -34,7 +35,11 @@ export default withAuth(config({
     db: {
         adapter: 'mongoose',
         url: databaseURL,
-        // data seeding
+        async onConnect(keystone) {
+            if (process.argv.includes('--seed-data')) {
+                await insertSeedData(keystone);
+            }
+        }
     },
     lists: createSchema({
         User,
