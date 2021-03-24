0c31304a024a4bd64f0b1f4e1eae9d8dc93d18b7
Michal Oktábec <info@michaloktabec.cz>
2021-03-24 18:04:59 +0100

    BE: Keystone config, first schemas including relationships, images from cloudinary

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,54 @@
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
+import 'dotenv/config';
+import { withItemData, statelessSessions } from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
+
+const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+    maxAge: 60 * 60 * 24 * 360,
+    secret: process.env.COOKIE_SECRET,
+}
+
+const { withAuth } = createAuth({
+    listKey: 'User',
+    identityField: 'email',
+    secretField: 'password',
+    initFirstItem: {
+        fields: ['name', 'email', 'password'],
+        // TODO : add roles
+    }
+})
+
+export default withAuth(config({
+    server: {
+        cors: {
+            origin: [process.env.FRONTEND_URL],
+            credentials: true,
+        }
+    },
+    db: {
+        adapter: 'mongoose',
+        url: databaseURL,
+        // data seeding
+    },
+    lists: createSchema({
+        User,
+        Product,
+        ProductImage,
+    }),
+    ui: {
+        // Show the UI only for people who pass this test
+        isAccessAllowed: ({ session }) => {
+            // console.log(session);
+            return !!session?.data;
+        },
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+        User: `id`
+    })
+}));
