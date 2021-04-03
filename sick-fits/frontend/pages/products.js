5f8754a1b88df221f379240dd9a34a0709c77ab9
Michal Oktábec <info@michaloktabec.cz>
2021-04-03 11:15:12 +0200

    Base Pagination component render

--- sick-fits/frontend/pages/products.js
+++ sick-fits/frontend/pages/products.js
@@ -1,8 +1,11 @@
 import Products from "../components/Products";
+import Pagination from "../components/Pagination";
 
 const ProductsPage = props => (
     <div>
+        <Pagination page={1} />
         <Products />
+        <Pagination page={1} />
     </div>
 );
 
