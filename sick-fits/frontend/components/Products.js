a1422d8ee9ad0fe1b78b4fb9a07569b297280a1c
Michal Oktábec <info@michaloktabec.cz>
2023-05-26 15:16:24 +0200

    Adding to Cart (FE) with animations

--- sick-fits/frontend/components/Products.js
+++ sick-fits/frontend/components/Products.js
@@ -5,49 +5,51 @@ import { perPage } from '../config';
 import Product from './Product';
 
 export const ALL_PRODUCTS_QUERY = gql`
-    query ALL_PRODUCTS_QUERY($skip: Int = 0, $first: Int) {
-        allProducts(first: $first, skip: $skip) {
-            id
-            name
-            price
-            description
-            photo {
-                id
-                image {
-                    publicUrlTransformed
-                }
-            }
+  query ALL_PRODUCTS_QUERY($skip: Int = 0, $first: Int) {
+    allProducts(first: $first, skip: $skip) {
+      id
+      name
+      price
+      description
+      photo {
+        id
+        image {
+          publicUrlTransformed
         }
+      }
     }
+  }
 `;
 
 const ProductsListStyles = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 60px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
 `;
 
 export default function Products({ page }) {
-    const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY, {
-        variables: {
-            skip: page * perPage - perPage,
-            first: perPage,
-        }
-    });
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY, {
+    variables: {
+      skip: page * perPage - perPage,
+      first: perPage,
+    },
+  });
 
-    if (loading) {
-        return <p>Loading...</p>;
-    }
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
-    if (error) {
-        return <p>Error: {error.message}</p>
-    }
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
 
-    return <div>
-        <ProductsListStyles>
-            {data.allProducts.map(product => (
-                <Product key={product.id} product={product} />
-            ))}
-        </ProductsListStyles>
+  return (
+    <div>
+      <ProductsListStyles>
+        {data.allProducts.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </ProductsListStyles>
     </div>
+  );
 }
