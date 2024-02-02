59ef293fb15b1876ecfb4658a374918564a5e30c
Michal Oktábec <info@michaloktabec.cz>
2024-02-02 15:35:34 +0100

    Finishing up the Checkout Flow

--- sick-fits/frontend/components/Checkout.js
+++ sick-fits/frontend/components/Checkout.js
@@ -6,6 +6,9 @@ import { useState } from "react";
 import nProgress from "nprogress";
 import gql from "graphql-tag";
 import { useMutation } from "@apollo/client";
+import { useRouter } from "next/dist/client/router";
+import { useCart } from "../lib/cartState";
+import { CURRENT_USER_QUERY } from "./User";
 
 const CheckoutFormStyles = styled.form`
     box-shadow: 0 1px 2px 2px rgba(0, 0, 0, 0.04);
@@ -37,8 +40,13 @@ function CheckoutForm() {
     const [loading, setLoading] = useState(false);
     const stripe = useStripe();
     const elements = useElements();
+    const router = useRouter();
+    const { closeCart } = useCart();
     const [checkout, { error: graphQLError }] = useMutation(
-        CREATE_ORDER_MUTATION
+        CREATE_ORDER_MUTATION,
+        {
+            refetchQueries: [{ query: CURRENT_USER_QUERY }],
+        }
     );
 
     async function handleSubmit(e) {
@@ -74,7 +82,14 @@ function CheckoutForm() {
         console.log(order);
 
         // 6. Change the page to view the order
+        router.push({
+            // pathname: '/order',
+            pathname: '/order/[id]',
+            query: { id: order.data.checkout.id }
+        })
+
         // 7. Close the cart
+        closeCart();
 
         // 8. Turn off the loader
         setLoading(false);
