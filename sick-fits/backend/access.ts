dd83769c9678599160838684b1f07b80a6e128c3
Michal Oktábec <info@michaloktabec.cz>
2024-03-15 14:21:40 +0100

    Permissions access functions

--- sick-fits/backend/access.ts
+++ sick-fits/backend/access.ts
@@ -1,6 +1,21 @@
+import { permissionsList } from "./schemas/fields";
 import { ListAccessArgs } from "./types";
 
 // access control returns yes/no value depending on the users session
 export function isSignedIn({ session }: ListAccessArgs) {
     return !!session;
 }
+
+const generatedPermissions = Object.fromEntries(
+    permissionsList.map(permission => [
+        permission,
+        function ({ session }: ListAccessArgs) {
+            return !!session?.data.role?.[permission];
+        },
+    ])
+);
+
+// permissions check if someone meets the criteria
+export const permissions = {
+    ...generatedPermissions
+};
