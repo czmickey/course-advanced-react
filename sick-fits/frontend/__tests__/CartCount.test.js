4e429e07cdb6b77e6817c67d609b2bd80df0ab67
Oktábec Michal <info@michaloktabec.cz>
2025-12-05 15:28:53 +0100

    Updating Props and re-rendering

---
+++ sick-fits/frontend/__tests__/CartCount.test.js
@@ -0,0 +1,25 @@
+import { render } from "@testing-library/react";
+import wait from "waait";
+import CartCount from '../components/CartCount';
+
+describe('<CartCount/>', () => {
+    it('Renders', () => {
+        render(<CartCount count={10}/>);
+    });
+    
+    it('Matches snapshot', () => {
+        const { container } = render(<CartCount count={11}/>);
+        expect(container).toMatchSnapshot();
+    });
+
+    it('Update via props', async () => {
+        const { container, rerender } = render(<CartCount count={11}/>);
+        expect(container).toHaveTextContent('11');
+        // Update the props
+        rerender(<CartCount count={12} />);
+        expect(container).toHaveTextContent('1211');
+        await wait(400);
+        expect(container).toHaveTextContent('12');
+        expect(container).toMatchSnapshot();
+    });
+});
